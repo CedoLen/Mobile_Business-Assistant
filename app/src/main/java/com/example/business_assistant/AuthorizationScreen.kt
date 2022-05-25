6a537d50e88af16df98f3caaee5d904f01cee237
Cedox <ps-2004@bk.ru>
2022-05-25 18:00:46 +0500

    Screen authorization and screen registration

--- app/src/main/java/com/example/business_assistant/AuthorizationScreen.kt
+++ app/src/main/java/com/example/business_assistant/AuthorizationScreen.kt
@@ -1,11 +1,18 @@
 package com.example.business_assistant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class AuthorizationScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.authorization_screen)
+    }
+
+    fun goToRegistration(view: View) {
+        val intent = Intent(this, RegistrationScreen::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/business_assistant/RegistrationScreen.kt
@@ -0,0 +1,11 @@
+package com.example.business_assistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registration_screen)
+    }
+}
